639a0b241cfcc55e2b95275ac0f5d5a879600dec
vishwa <mukulk08@gmail.com>
2014-09-16 23:35:25 +0530

    second assignment modified

--- README.txt
+++ README.txt
@@ -1,12 +1,12 @@
 Name: Mukul M
 USN: 1PI12IS059
 Q.NO:1
-Q.Title: Calculating the net profit or loss from a .csv file containing the selling price and cost price of all the items.
+Q.Title: Calculating the net profit or loss from a .csv file containing the selling price and cost price of all the items based on either used or unused.(modified)
 
      The source code files that are being submitted are assn_2.c and cal.csv.
      The instructions to be followed while executing the code are 
         1) compile the program using gcc assn_2.c
-        2) ./a.out -f cal.csv -l log.txt
+        2) ./a.out -f call.csv -l log.txt
      
      log.txt is the any log file that will be created on execution.
-        The file is being processed character by character and the logic is that the characters after the first comma is encountered are stored in an array and this is converted into an integer using a standard function atoi().The next number is extracted in a similar way till the \n is encountered.This gives CP and SP .Every time the CP and SP value gets added to the previous values.Finally the values of CP and SP are subtracted to give whether it is profit or loss. 
+        The file is being processed character by character and the logic is that the characters after the first comma is encountered are stored in an array and this is converted into an integer using a standard function atoi().The next number is extracted in a similar way till the next comma is encountered.This gives CP and SP .After this the next word is taken into an array of characters.Then the second letter in used or unused is compared with either 's' if used or 'n' if unused.Based on the comparison ,the computation is done differently.Every time the CP and SP value gets added to the previous values in their respective used or unused blocks.Finally the values of CP and SP are subtracted to give whether it is profit or loss. 
--- asn2_1.c
+++ asn2_1.c
@@ -3,34 +3,32 @@ USN: 1PI12IS059,1PI12IS069
 Q.NO:1
 Q.Title: Calculating the net profit or loss from a .csv file containing the selling price and cost price of all the items.
 
-  Functions: 
-  				getopt()
-            	fgetc()
-  				gettime()
-             	atoi()
-             	
+  Functions: gettime()
+             getopt()
+             fgetc()
+             atoi()
   Parameters: argval1 and argval2 are the two parameters that are accepted from the user which are the .csv and log files.
 */
 
 
- // header files
+/** header files **/
+
+
 #include <stdio.h>
 #include<time.h>
 #include<unistd.h>
 char timebuf[50];
 int gettime();
 
-
-// main function
-
+/** main function **/
 
 int main (int argc,char *argv[])
 {
-   int k;
-   char *argval1=NULL;
-   char *argval2=NULL;
+  int k;
+  char *argval1=NULL;
+  char *argval2=NULL;
    while((k=getopt(argc,argv,"f:l:"))!=-1)
-   switch(k)
+     switch(k)
     {
       case 'f': argval1=optarg;
                  break;
@@ -41,28 +39,31 @@ int main (int argc,char *argv[])
               printf(" INVALID ARGUMENTS \n");
                return 1;
      }
-   	FILE *logfile = fopen(argval2, "a" );
+   FILE *logfile = fopen(argval2, "a" );
     FILE *fp;
-   	int c;
-   	int ret;
-   	char buf[30];
-	int cp, sp,i,j;
-	int tot_cp=0;
-	int tot_sp=0;
-	char co[99],so[99];
-	char char1;
-	int result;
+   int c;
+   int ret;
+   char buf[30];
+int cp,sp;
+int used_cp, used_sp,unused_cp, unused_sp,i,j,z;
+int tot_used_cp=0;
+int tot_unused_cp=0;
+int tot_used_sp=0;
+int tot_unused_sp=0;
+char co[99],so[99],ty[999];
+char char1;
+int result;
 		if(argc != 1)
 		{
-          printf( "Filename: %s \n", argval1 );
-        }
+        		 printf( "Filename: %s \n", argval1 );
+    	        }
     	
     	
     	
    fp = fopen(argval1,"r");
    if(fp == NULL)
    {
-      printf("Error in opening file \n");
+      printf("Error in opening file");
       return(-1);
    }
 while(1)
@@ -73,35 +74,56 @@ while(1)
 	if(c==',')
 	{	
 		i=0;
-  		do{
+  			do
+  			{
       		   c = fgetc(fp);
       	       	   co[i]=c;
-		   i++;
-    		   } while(c!=',');		
-      			
-      			cp=atoi(co);
-				tot_cp=tot_cp+cp;		
-		
+		   			i++;
+    		   }while(c!=',');	
+    		   	
+      		cp=atoi(co);		
                 j=0;
+                
   		do
   		{
-      		 c = fgetc(fp);
-      		 so[j]=c;
-		  	 j++;
-    	 }while(c!='\n');
-    	 		
+      		   c = fgetc(fp);
+      		   so[j]=c;
+		  	    j++;
+    	}while(c!=',');	
+    		
       		   sp=atoi(so);
-    		   tot_sp=tot_sp+sp;	
+                z=0;
+                
+                   do
+                   {
+      		   			c = fgetc(fp);
+      		   			ty[z]=c;
+		   				z++;
+    		   	    }while(c!='\n');
+    		   	    		
+      		   if(ty[2]=='s')
+                    {
+                       used_cp=cp;
+                       used_sp=sp;
+    		        	tot_used_cp=tot_used_cp+used_cp;
+                        tot_used_sp=tot_used_sp+used_sp;
+                    }
+                    else if(ty[2]=='n')
+                    {
+                       unused_cp=cp;
+                       unused_sp=sp;
+    		        	tot_unused_cp=tot_unused_cp+unused_cp;
+                        tot_unused_sp=tot_unused_sp+unused_sp;
+                    }
+                    	
 		
 	}
 
 }
 
+/** checking for profit in the case of used items **/
 
-/**checking if its profit **/
-
-
-if(tot_sp > tot_cp)
+if(tot_used_sp > tot_used_cp)
 	{
 	  	ret=gettime();
 		
@@ -116,17 +138,14 @@ if(tot_sp > tot_cp)
                 }
               fprintf(logfile,"%c",char1);
            }		
-		fprintf(logfile, "%s net profit = %d \n\n\n\n\n\n", timebuf, tot_sp-tot_cp);
-		printf("net profit = %d \n", tot_sp-tot_cp);
+		fprintf(logfile, "%s net profit of used items = %d \n\n\n", timebuf, tot_used_sp-tot_used_cp);
+		printf("net profit of used items = %d \n", tot_used_sp-tot_used_cp);
 		
 	}
 	
+/** checking for loss in the case of used items **/
 	
-	
-/** checking if its loss **/
-
-
-else if(tot_cp>tot_sp)
+ if(tot_used_cp>tot_used_sp)
 	{
   	ret=gettime();
          FILE *fp1;
@@ -139,18 +158,68 @@ else if(tot_cp>tot_sp)
                  break;
                 }
               fprintf(logfile,"%c",char1);
-            }
+           }
 
-		fprintf(logfile, "%s net loss = %d \n\n\n\n\n\n", timebuf, tot_cp-tot_sp);
-		printf(" net loss = %d \n", tot_cp-tot_sp);
-	 }
-		else
-        {
-   	 		ret=gettime();
-			fprintf(logfile, "%s net loss or profit= %d\n\n\n\n\n\n", timebuf, tot_cp-tot_sp);
-			printf(" net loss or profit = %d", tot_cp-tot_sp);
-        }
+	fprintf(logfile, "%s net loss = %d \n\n\n", timebuf, tot_used_cp-tot_used_sp);
+	printf(" net loss = %d \n", tot_used_cp-tot_used_sp);
+	}
+ if(tot_used_cp==tot_used_sp)
+       {
+   	 ret=gettime();
+	fprintf(logfile, "%s net loss or profit of the used items= %d\n\n\n\n", timebuf, tot_used_cp-tot_used_sp);
+	printf(" net loss or profit of the used items= %d", tot_used_cp-tot_used_sp);
+       }
 	
+/** checking for profit in the case of un-used items **/
+
+
+    if(tot_unused_sp > tot_unused_cp)
+	{
+	  	ret=gettime();
+		
+		   FILE *fp2;
+         fp2=fopen(argval1,"r");
+      //    while(1)
+        //   {
+        //     char1=fgetc(fp2);
+       //      if(char1==EOF)
+       //        {
+      //           break;
+      //          }
+      //        fprintf(logfile,"%c",char1);
+       //    }		
+		fprintf(logfile, "%s net profit of brand new items= %d \n\n\n\n\n\n", timebuf, tot_unused_sp-tot_unused_cp);
+		printf("net profit of brand new items = %d \n", tot_unused_sp-tot_unused_cp);
+		
+	}
+	
+/** checking for loss in the case of un-used items **/
+
+
+ if(tot_unused_cp>tot_unused_sp)
+	{
+  	ret=gettime();
+         FILE *fp2;
+         fp2=fopen(argval1,"r");
+     //      while(1)
+       //    {
+         //    char1=fgetc(fp2);
+         //    if(char1==EOF)
+         //      {
+         //        break;
+         //       }
+        //      fprintf(logfile,"%c",char1);
+       //    }
+
+	fprintf(logfile, "%s net loss of brand new items = %d \n\n\n\n\n\n", timebuf, tot_unused_cp-tot_unused_sp);
+	printf(" net loss of brand new items = %d \n", tot_unused_cp-tot_unused_sp);
+	}
+ if(tot_unused_cp==tot_unused_sp)
+       {
+   	 ret=gettime();
+	fprintf(logfile, "%s net loss or profit of the unused items= %d\n\n\n\n\n\n", timebuf, tot_unused_cp-tot_unused_sp);
+	printf(" net loss or profit of the unused items= %d", tot_unused_cp-tot_unused_sp);
+       }
       
    fclose(fp);
    return(0);
@@ -168,4 +237,4 @@ else if(tot_cp>tot_sp)
     			tm_info = localtime(&timer);
     			strftime(timebuf, 25, "[%d:%m:%Y %H:%M:%S]", tm_info);
     			return 0;
-   		    }
+   		 }
